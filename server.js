7857cef043b1fcbf9e99fdc4324aa7c0a8db9426
hatembenmahjouba / inter-maghreb <ben.mahjouba.hatem@gmail.com>
2022-09-24 01:07:22 +0200

    connecting with database from our server

--- server.js
+++ server.js
@@ -11,6 +11,15 @@ app.use(express.json());
 app.use(cors());
 
 const server = http.createServer(app);
-server.listen(PORT, () => {
-  console.log(`Server is listening on ${PORT}`);
-});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server is listening on ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log('database connection failed. Server not started');
+    console.error(err);
+  });
