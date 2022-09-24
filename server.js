bf1238f25f0b0263978bf0b91d94abd0095a1c35
hatembenmahjouba / inter-maghreb <ben.mahjouba.hatem@gmail.com>
2022-09-24 14:00:29 +0200

    adding authentification

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
+require('dotenv').config();
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
 const mongoose = require('mongoose');
-require('dotenv').config();
+
+const authRoutes = require('./routes/authRoutes');
 
 const PORT = process.env.PORT || process.env.API_PORT;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use('/api/auth', authRoutes);
 
 const server = http.createServer(app);
 
