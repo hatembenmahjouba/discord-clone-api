d2639f2a80a518636aecaf74220cfcb2595348d8
hatembenmahjouba / inter-maghreb <ben.mahjouba.hatem@gmail.com>
2022-10-02 01:03:17 +0200

    connectiong to socket.io

--- socketServer.js
+++ socketServer.js
@@ -1,11 +1,22 @@
+const authSocket = require('./middleware/authSocket');
+const newConnectionHandler = require('./socketHandlers/newConnectionHandler');
+
 const registerSocketServer = (server) => {
   const io = require('socket.io')(server, {
-    cors: { origin: '*', methods: ['GET', 'POST'] },
+    cors: {
+      origin: '*',
+      methods: ['GET', 'POST'],
+    },
+  });
+
+  io.use((socket, next) => {
+    authSocket(socket, next);
   });
 
   io.on('connection', (socket) => {
     console.log('user connected');
     console.log(socket.id);
+    newConnectionHandler(socket, io);
   });
 };
 
